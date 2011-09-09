24ea0e3bee47f6eec33fd235ab2c8162186c5644
Alexander Kojevnikov <alexander@kojevnikov.com>
2011-09-09 20:15:29 +0800

    Clean up

--- db/muspy.sql
+++ db/muspy.sql
@@ -1,25 +1,3 @@
-CREATE TABLE "auth_group" (
-    "id" integer NOT NULL PRIMARY KEY,
-    "name" varchar(80) NOT NULL UNIQUE
-);
-CREATE TABLE "auth_group_permissions" (
-    "id" integer NOT NULL PRIMARY KEY,
-    "group_id" integer NOT NULL,
-    "permission_id" integer NOT NULL REFERENCES "auth_permission" ("id"),
-    UNIQUE ("group_id", "permission_id")
-);
-CREATE TABLE "auth_message" (
-    "id" integer NOT NULL PRIMARY KEY,
-    "user_id" integer NOT NULL REFERENCES "auth_user" ("id"),
-    "message" text NOT NULL
-);
-CREATE TABLE "auth_permission" (
-    "id" integer NOT NULL PRIMARY KEY,
-    "name" varchar(50) NOT NULL,
-    "content_type_id" integer NOT NULL,
-    "codename" varchar(100) NOT NULL,
-    UNIQUE ("content_type_id", "codename")
-);
 CREATE TABLE "auth_user" (
     "id" integer NOT NULL PRIMARY KEY,
     "username" varchar(30) NOT NULL UNIQUE,
@@ -33,41 +11,9 @@ CREATE TABLE "auth_user" (
     "last_login" datetime NOT NULL,
     "date_joined" datetime NOT NULL
 );
-CREATE TABLE "auth_user_groups" (
-    "id" integer NOT NULL PRIMARY KEY,
-    "user_id" integer NOT NULL,
-    "group_id" integer NOT NULL REFERENCES "auth_group" ("id"),
-    UNIQUE ("user_id", "group_id")
-);
-CREATE TABLE "auth_user_user_permissions" (
-    "id" integer NOT NULL PRIMARY KEY,
-    "user_id" integer NOT NULL,
-    "permission_id" integer NOT NULL REFERENCES "auth_permission" ("id"),
-    UNIQUE ("user_id", "permission_id")
-);
-CREATE TABLE "django_content_type" (
-    "id" integer NOT NULL PRIMARY KEY,
-    "name" varchar(100) NOT NULL,
-    "app_label" varchar(100) NOT NULL,
-    "model" varchar(100) NOT NULL,
-    UNIQUE ("app_label", "model")
-);
 CREATE TABLE "django_session" (
     "session_key" varchar(40) NOT NULL PRIMARY KEY,
     "session_data" text NOT NULL,
     "expire_date" datetime NOT NULL
 );
-CREATE TABLE "django_site" (
-    "id" integer NOT NULL PRIMARY KEY,
-    "domain" varchar(100) NOT NULL,
-    "name" varchar(50) NOT NULL
-);
-CREATE INDEX "auth_group_permissions_1e014c8f" ON "auth_group_permissions" ("permission_id");
-CREATE INDEX "auth_group_permissions_bda51c3c" ON "auth_group_permissions" ("group_id");
-CREATE INDEX "auth_message_fbfc09f1" ON "auth_message" ("user_id");
-CREATE INDEX "auth_permission_e4470c6e" ON "auth_permission" ("content_type_id");
-CREATE INDEX "auth_user_groups_bda51c3c" ON "auth_user_groups" ("group_id");
-CREATE INDEX "auth_user_groups_fbfc09f1" ON "auth_user_groups" ("user_id");
-CREATE INDEX "auth_user_user_permissions_1e014c8f" ON "auth_user_user_permissions" ("permission_id");
-CREATE INDEX "auth_user_user_permissions_fbfc09f1" ON "auth_user_user_permissions" ("user_id");
 CREATE INDEX "django_session_c25c2c28" ON "django_session" ("expire_date");
--- settings.py
+++ settings.py
@@ -1,12 +1,9 @@
-# Django settings for muspy project.
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     ('admin', 'admin@muspy.com'),
 )
-
 MANAGERS = ADMINS
 
 DATABASES = {
@@ -16,35 +13,22 @@ DATABASES = {
     }
 }
 
-TIME_ZONE = ''
-
-LANGUAGE_CODE = 'en-GB'
-
-SITE_ID = 1
-
+TIME_ZONE = None
+LANGUAGE_CODE = 'en-gb'
 USE_I18N = False
-USE_L10N = True
+USE_L10N = False
 
 MEDIA_ROOT = ''
 MEDIA_URL = ''
-STATIC_ROOT = ''
 STATIC_URL = '/static/'
-ADMIN_MEDIA_PREFIX = '/static/admin/'
 STATICFILES_DIRS = (
     'static',
-)
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 SECRET_KEY = '^c)@qv0@pb$ym&zb^#vm8nuv0972qa9w(#le&atirpvfvi_yjc'
 
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -63,13 +47,9 @@ TEMPLATE_DIRS = (
 
 INSTALLED_APPS = (
     'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django.contrib.admin',
-    # 'django.contrib.admindocs',
 )
 
 LOGGING = {
