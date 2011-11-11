f5ae90b9535a21179375b0ad28186814ba342ae1
Alexander Kojevnikov <alexander@kojevnikov.com>
2011-11-11 08:20:06 +0800

    daemon: Send notifications in order

--- daemon/notifications.py
+++ daemon/notifications.py
@@ -28,7 +28,7 @@ def send():
         jobs.process()
         tools.sleep()
         try:
-            notification = Notification.objects.all()[0]
+            notification = Notification.objects.order_by('user_id')[0]
         except IndexError:
             break # last one
 
