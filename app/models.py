e98d0841791f113be2b8f1cdddb7c44a574fb5db
Alexander Kojevnikov <alexander@kojevnikov.com>
2013-06-26 09:43:37 -0700

    Add another V/A

--- app/models.py
+++ app/models.py
@@ -48,6 +48,7 @@ class Artist(models.Model):
         'b7c7dfd9-d735-4733-9b10-f060ac75bd6a',
         'b05cc773-4e8e-40bc-ae12-dc88dfc2c9ec',
         '4b2228f5-e18b-4acc-ace7-b8db13a9306f',
+        '046c889d-5b1c-4f54-9c7b-319a8f67e729',
         'f731ccc4-e22a-43af-a747-64213329e088', # [anonymous]
         '33cf029c-63b0-41a0-9855-be2a3665fb3b', # [data]
         '314e1c25-dde7-4e4d-b2f4-0a7b9f7c56dc', # [dialogue]
